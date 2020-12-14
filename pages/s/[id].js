fac2a44e148d904a03d305dee851c1529849e25a
Teaseung Kim <teaseungkim@gmail.com>
2020-12-14 08:50:10 -0600

    Update [id].js
    fixed spacing

--- pages/s/[id].js
+++ pages/s/[id].js
@@ -4,27 +4,27 @@ import styles from '../../styles/Redirect.module.css';
 const apiEndpoint = `http://localhost:3000/api/url`
 
 async function fetchRedirect(apiPath, id) {
-	const path = `${apiPath}/${id}`;
-	const response = await fetch(path);
-	const { redirect: url, error } = await response.json();
-	return url
+  const path = `${apiPath}/${id}`;
+  const response = await fetch(path);
+  const { redirect: url, error } = await response.json();
+  return url
 };
 
 function RedirectUrl () { 
-	return (
-		<div className={styles.redirect}>Redirecting...</div>
-	);
+  return (
+    <div className={styles.redirect}>Redirecting...</div>
+  );
 }
 
 RedirectUrl.getInitialProps = async ({ query: { id }, res }) => {
-	const newUrl = await fetchRedirect(apiEndpoint, id)
-	if(res) {
-		res.writeHead(302, { Location: newUrl })
-		res.end();
-	} else {
-		window.location = newUrl;
-	}
-	return {};
+  const newUrl = await fetchRedirect(apiEndpoint, id)
+  if(res) {
+    res.writeHead(302, { Location: newUrl })
+    res.end();
+  } else {
+    window.location = newUrl;
+  }
+  return {};
 }
 
 export default RedirectUrl
