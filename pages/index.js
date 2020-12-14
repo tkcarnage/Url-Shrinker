0bda516f1b07b8f72178611c0f95509a6c30bf5f
TK <teaseungkim@gmail.com>
2020-12-14 06:28:23 -0600

    changed title

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Url Shrinker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
