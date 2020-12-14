5da7f57ebd2514ef2051456d006cb1d82f32781f
TK <teaseungkim@gmail.com>
2020-12-14 08:16:46 -0600

    vercel deployment

--- pages/s/[id].js
+++ pages/s/[id].js
@@ -4,7 +4,7 @@ import styles from '../../styles/Redirect.module.css';
 const apiEndpoint = `api/url`
 
 async function fetchRedirect(apiPath, id) {
-	console.log('calling fetchRedirect', apiPath, apiEndpoint)
+	console.log('calling fetchRedirect', apiPath, id);
 	const path = `${apiPath}/${id}`;
 	const response = await fetch(path);
 	const { redirect: url, error } = await response.json();
@@ -18,6 +18,7 @@ function RedirectUrl () {
 }
 
 RedirectUrl.getInitialProps = async ({ query: { id }, res }) => {
+	console.log('redirect?')
 	const newUrl = await fetchRedirect(apiEndpoint, id)
 	if(res) {
 		res.writeHead(302, { Location: newUrl })
